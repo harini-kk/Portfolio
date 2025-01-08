b0a9ac7a3b2c7a07a727bb9ad88f6dc13dfc2b72
harini-kk <hkarthikeyan@fleetstudio.com>
2025-01-08 17:44:43 +0530

    form data commit

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Button from "../reusable/Button";
-import FormInput from "../reusable/FormInput";
 
 const ContactForm = () => {
 	const [formData, setFormData] = useState({
@@ -16,7 +15,7 @@ const ContactForm = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log("Form Data Submitted:", formData); // Debugging
+		console.log("Form Data Submitted:", formData);
 
 		const { name, email, message } = formData;
 
@@ -50,41 +49,57 @@ const ContactForm = () => {
 					<p className="font-general-medium text-primary-dark dark:text-primary-light text-2xl mb-8">
 						Contact Form
 					</p>
-					<FormInput
-						inputLabel="Full Name"
-						labelFor="name"
-						inputType="text"
-						inputId="name"
-						inputName="name"
-						placeholderText="Your Name"
-						ariaLabelName="Name"
-						onChange={handleInputChange}
-					/>
-					<FormInput
-						inputLabel="Email"
-						labelFor="email"
-						inputType="email"
-						inputId="email"
-						inputName="email"
-						placeholderText="Your email"
-						ariaLabelName="Email"
-						onChange={handleInputChange}
-					/>
 
-					<div className="mt-6">
+					<div className="mb-4">
 						<label
+							htmlFor="name"
 							className="block text-lg text-primary-dark dark:text-primary-light mb-2"
+						>
+							Full Name
+						</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							placeholder="Your Name"
+							aria-label="Name"
+							className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
+							onChange={handleInputChange}
+						/>
+					</div>
+
+					<div className="mb-4">
+						<label
+							htmlFor="email"
+							className="block text-lg text-primary-dark dark:text-primary-light mb-2"
+						>
+							Email
+						</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							placeholder="Your Email"
+							aria-label="Email"
+							className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
+							onChange={handleInputChange}
+						/>
+					</div>
+
+					<div className="mb-4">
+						<label
 							htmlFor="message"
+							className="block text-lg text-primary-dark dark:text-primary-light mb-2"
 						>
 							Message
 						</label>
 						<textarea
-							className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
 							id="message"
 							name="message"
-							cols="14"
-							rows="6"
+							placeholder="Your Message"
 							aria-label="Message"
+							rows="6"
+							className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
 							onChange={handleInputChange}
 						></textarea>
 					</div>
